ed7dce4846956442ebd7ce2d42a86b63f126b362
Luis Franco R <luisfrancor@Luis-iMac.local>
2020-05-22 18:40:22 +0200

    initial xcode project creation

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by Luis Franco R on 22/05/2020.
+//  Copyright © 2020 Luis Franco R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
